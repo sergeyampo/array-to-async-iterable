263bfaf8589ef3d335ec17851e994918a894be2d
sergeyampo <K7p4B5b5GX711>
2023-01-20 10:38:26 +0300

    Typings fix

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,6 @@ export declare class AsyncTimeIterator{
      * @param array Iterable contract.
      * @param ms Give a quantum of time to the event loop every n milliseconds.
      */
-    constructor<T>(array: Array<T>, ms?: number);
+    constructor(array: Array<any>, ms?: number);
+    [Symbol.asyncIterator](): AsyncIterator<any>;
 }
